0ad7434704214bf3887b2812b884e2d072cb824e
ABUCKY0 <81783950+ABUCKY0@users.noreply.github.com>
2024-05-29 14:41:47 -0500

    Rewrite to be more similar to existing workflow

--- build-tools/build.sh
+++ build-tools/build.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 git config --global --add safe.directory /github/workspace
-# Fetching Project Information
+
+# ----------------
+# CHECK IF TEMPLATE
+# ----------------
+
+template=$(awk -F'=' '/^IS_LIBRARY:=/{print $2}' Makefile)
+echo $template
+echo "template=$template" >> $GITHUB_OUTPUT
+
+
+# ----------------
+# GET PROJECT INFO
+# ----------------
+
 if [ "$ACTION" == "opened" ]; then
     # Fetch the head SHA directly from the PR API
     API_URL="https://api.github.com/repos/$REPOSITORY/pulls/$PR_NUM"
@@ -9,52 +22,87 @@ else
     # Use the commit SHA after the event
     sha=$(git rev-parse HEAD | head -c 6)
 fi
-
-# Printing SHA for debugging
+echo "sha=$sha" >> $GITHUB_OUTPUT
 echo "SHA found: $sha"
 
-# Update version in Makefile
-makefile_version=$(awk -F'=' '/^VERSION:=/{print $2}' Makefile)
-sed -i "s/^VERSION:=.*\$/VERSION:=$makefile_version/" Makefile
+version=$(awk -F'=' '/^VERSION:=/{print $2}' Makefile)
+echo "Version found: $version"
+echo "version=$version" >> "$GITHUB_OUTPUT"
 
-# present in makefile
-library_name=$(awk -F'=' '/^LIBRARY_NAME:=/{print $2}' Makefile)
-# github sha short
-postfix = $(git rev-parse --short HEAD)
+postfix="$version+$sha"
+echo "postfix=$postfix" >> "$GITHUB_OUTPUT"
+echo "Postfix found: $postfix"
 
-# Making Template
+library_name=$(awk -F'=' '/^LIBNAME:=/{print $2}' Makefile)
+echo "library_name=$library_name" >> "$GITHUB_OUTPUT"
+echo "Library name found: $library_name"
+
+name="$library_name@$postfix"
+echo "name=$name" >> "$GITHUB_OUTPUT"
+echo "Name found: $name"
+
+# ----------------
+# BUILDING PROJECT
+# ----------------
+
 make clean quick -j
-pros make template
 
-# Unzipping Template
-template_name="$library_name@$postfix"
-echo $template_name
-unzip "$template_name.zip" -d template
+# ----------------
+# CREATING TEMPLATE
+# ----------------
+# if: ${{ steps.template.outputs.template == 1 && inputs.library-path != null }}
 
-# Upload Artifact
-if [ -n "$library_path" ]; then
-    echo "Uploading Artifact"
-    artifact_dir="/github/workspace/template/include/$library_path"
-    mkdir -p "$artifact_dir"
-    
-    # Copying necessary files
-    cp {LICENSE*,README*} "$artifact_dir"/
-    
-    # Adding GitHub link to README
-    readme_path="$artifact_dir/README.md"
-    echo -e "\n## [Github link]($GITHUB_SERVER_URL/$REPOSITORY)" >> "$readme_path" 
-    perl -i -pe 's@(?<=[^/])(docs/assets/.*?)(?=[")])@${GITHUB_SERVER_URL}/${REPOSITORY}/blob/master/$1?raw=true@g' "$readme_path" # I'm not smart enough for this, was aided by ChatGPT
-    
-    # Writing version info
-    echo "$postfix" >> "$artifact_dir/VERSION"
-
-    # Zipping and moving to workspace
-    cd /github/workspace/template
-    zip -r "$template_name.zip"
-    echo $template_name + ".zip" >> $GITHUB_OUTPUT
-    echo $CWD
-    # mv "$template_name.zip" /
-    ls -a
-    # # Uploading Artifact
-    # node upload.js "/$template_name.zip"
+if [ "$template" == "1" ]; then
+    echo "Creating template"
+    pros make template
+    template_name="$library_name@$postfix"
+    echo "template_name=$template_name" >> "$GITHUB_OUTPUT"
+    echo "Template created: $template_name"
+    unzip "$template_name.zip" -d template
 fi
+
+# # Update version in Makefile
+# makefile_version=$(awk -F'=' '/^VERSION:=/{print $2}' Makefile)
+
+# version=$(awk -F'=' '/^VERSION:=/{print $2}' Makefile)
+# # present in makefile
+# library_name=$(awk -F'=' '/^LIBRARY_NAME:=/{print $2}' Makefile)
+# # github sha short
+# # postfix=$(git rev-parse --short HEAD)
+# postfix="$version+$sha"
+# # Making Template
+# make clean quick -j
+# pros make template
+
+# # Unzipping Template
+# template_name="$library_name@$postfix"
+# echo $template_name
+# unzip "$template_name.zip" -d template
+
+# # Upload Artifact
+# if [ -n "$library_path" ]; then
+#     echo "Uploading Artifact"
+#     artifact_dir="/github/workspace/template/include/$library_path"
+#     mkdir -p "$artifact_dir"
+    
+#     # Copying necessary files
+#     cp {LICENSE*,README*} "$artifact_dir"/
+    
+#     # Adding GitHub link to README
+#     readme_path="$artifact_dir/README.md"
+#     echo -e "\n## [Github link]($GITHUB_SERVER_URL/$REPOSITORY)" >> "$readme_path" 
+#     perl -i -pe 's@(?<=[^/])(docs/assets/.*?)(?=[")])@${GITHUB_SERVER_URL}/${REPOSITORY}/blob/master/$1?raw=true@g' "$readme_path" # I'm not smart enough for this, was aided by ChatGPT
+    
+#     # Writing version info
+#     echo "$postfix" >> "$artifact_dir/VERSION"
+
+#     # Zipping and moving to workspace
+#     cd /github/workspace/template
+#     zip -r "$template_name.zip"
+#     echo $template_name + ".zip" >> $GITHUB_OUTPUT
+#     echo $CWD
+#     # mv "$template_name.zip" /
+#     ls -a
+#     # # Uploading Artifact
+#     # node upload.js "/$template_name.zip"
+# fi
